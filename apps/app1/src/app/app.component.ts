5afdf98ba817494e13198cee82801be373fe2824
Navaneeth Reddy <thutinavaneethreddy@gmail.com>
2021-09-28 11:46:42 +0530

    initial commit

---
+++ apps/app1/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'minimal-generators-unit-testing-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'app1';
+}
